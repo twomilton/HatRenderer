9a09b5e11293f4e5c8fbbf07b090b818f2130b2b
twomilton <twomilton@gmail.com>
2026-08-13 14:31:24 -0500

    Add GLFW dependency

--- HatRenderer.cpp
+++ HatRenderer.cpp
@@ -1,6 +1,24 @@
 ﻿// HatRenderer.cpp : Defines the entry point for the application.
 //
 
+
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+
+
+
+/*
 #include "HatRenderer.h"
 #include <iostream>
 
@@ -11,3 +29,4 @@ int main()
 	std::cout << "HatRenderer starting...\n";
 	return 0;
 }
+*/
