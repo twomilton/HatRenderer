31b244a20d0c12ad54bfe3016a67afc7f8b5ab58
twomilton <twomilton@gmail.com>
2026-08-13 13:49:24 -0500

    Create initial CMake application

---
+++ HatRenderer.cpp
@@ -0,0 +1,13 @@
+﻿// HatRenderer.cpp : Defines the entry point for the application.
+//
+
+#include "HatRenderer.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	std::cout << "HatRenderer starting...\n";
+	return 0;
+}
