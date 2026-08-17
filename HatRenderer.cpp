0ba123699d33691f20360dba538da50081103d82
twomilton <twomilton@gmail.com>
2026-08-17 00:15:26 -0500

    Initialize OpenGL context

--- HatRenderer.cpp
+++ HatRenderer.cpp
@@ -2,31 +2,85 @@
 //
 
 
+#include <iostream>
 #include <GLFW/glfw3.h>
+
+void glfwErrorCallback(int error, const char* description)
+{
+	std::cerr << "GLFW Error " << error << ": "
+		<< description << '\n';
+}
+
 
 int main()
 {
+	glfwSetErrorCallback(glfwErrorCallback);
+
+	std::cout << "Starting HatRenderer...\n";
+
 	if (!glfwInit())
 	{
 		return -1;
 	}
 
+	std::cout << "GLFW initialized. \n";
+
+	// Tell GLFW which version of OpenGL we want.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	std::cout << "Creating OpenGL window...\n";
+
+	// Create the window.
+	GLFWwindow* window = glfwCreateWindow(
+		1280,
+		720,
+		"HatRenderer",
+		nullptr,
+		nullptr
+	);
+
+	if (window == nullptr)
+	{
+		std::cerr << "ERROR: glfwCreateWindow() failed.\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	std::cout << "Window created successfully!\n";
+
+	// Make the OpenGL context current.
+	glfwMakeContextCurrent(window);
+
+	const GLubyte* vendor = glGetString(GL_VENDOR);
+	const GLubyte* renderer = glGetString(GL_RENDERER);
+	const GLubyte* version = glGetString(GL_VERSION);
+
+	std::cout << "OpenGL Vender:	"
+		<< vendor << '\n';
+
+	std::cout << "OpenGL Renderer:	"
+		<< renderer << '\n';
+
+	std::cout << "OpenGL Version:	"
+		<< version << '\n';
+
+
+	std::cout << "OpenGL Context created successfully!\n";
+
+	// Main app loop.
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Clean up.
+	glfwDestroyWindow(window);
 	glfwTerminate();
+
+	std::cout << "HatRenderer shutting down.\n";
+	
 	return 0;
 }
-
-
-
-
-/*
-#include "HatRenderer.h"
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	std::cout << "HatRenderer starting...\n";
-	return 0;
-}
-*/
--- README.md
+++ README.md
@@ -4,3 +4,20 @@ Current milestone:
 ✔ Repository created
 Next goal:
 Render one triangle.
+
+## Hardware
+
+HatRenderer is currently being developed on:
+
+	- CPU: Intel core i7-4710HQ
+	- RAM: 16 GB
+	- GPU: NVIDIA GeForce GTX 860M
+	- Integrated GPU: Intel HD Graphics 4600
+	- Storage: SSD
+	- OS: Windows 10
+
+	Current OpenGL context:
+
+	- Vendor: Intel
+	- Renderer: Intel HD Graphics 4600
+	- Version: OpenGL 3.3
