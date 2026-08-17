f0036def1b53a1dd75c4c7672457d4c7be1d7b56
twomilton <twomilton@gmail.com>
2026-08-17 00:36:04 -0500

    Render clear color

--- HatRenderer.cpp
+++ HatRenderer.cpp
@@ -72,6 +72,9 @@ int main()
 	// Main app loop.
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
